56a91d1c6824c52ff77cad60a8870ac952c17ae5
xzyl4303 <115838895+xzyl4303@users.noreply.github.com>
2023-10-17 09:08:57 -0600

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function toggleSidebar() {
-  var sidebar = document.querySelector('.sidebar');
-  var content = document.querySelector('.content');
-
-  sidebar.classList.toggle('open');
-  content.classList.toggle('shrink');
-}
